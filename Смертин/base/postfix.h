f1f9b71db2856fb3e6acd00ae74bbc4cac176310
Death-Boy-Dmi <smertindmi@mail.ru>
2016-11-11 21:10:03 +0300

    class TablFunc

--- Смертин/base/postfix.h
+++ Смертин/base/postfix.h
@@ -6,6 +6,37 @@
 
 using namespace std;
 
+class TablFunc
+{
+	char func[6];
+	int priority[6];
+	int amount;
+public:
+	TablFunc()
+	{
+		func[0] = ')';	amount = 2;
+		func[1] = '(';	priority[1] = 0;
+		func[2] = '+';	priority[2] = 1;
+		func[3] = '-';	priority[3] = 1;
+		func[4] = '*';	priority[4] = 2;
+		func[5] = '/';	priority[5] = 2;
+	}
+	bool ChekAmount(string str)
+	{
+		string arop = "-*/+";
+		string var = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		for (int i = 0; i < str.length; i++)
+			if (arop.find(str[i]) != std::string::npos)
+				if (var.find(str[(arop.find(str[i])) - 1]) != std::string::npos
+					&& var.find(str[(arop.find(str[i])) + 1]) != std::string::npos)
+					return true;
+				else
+					return false;
+			else
+				throw "The arithmetic operation is not found";
+	}
+};
+
 class TPostfix
 {
   string infix;
