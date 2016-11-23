f3fdf74726a586c8368054964519ccde6c67a37e
Death-Boy-Dmi <smertindmi@mail.ru>
2016-11-23 16:00:38 +0300

    Добавил Тесты и исправил Postfix.h

--- Смертин/base/postfix.h
+++ Смертин/base/postfix.h
@@ -35,10 +35,10 @@ class TPostfix
 	TablFunc functions;
 
 public:
-	TPostfix()
+	TPostfix(string inf)
 	{
-		cout << "Enter the expression (variable names - Latin letters):" << endl;
-		cin >> infix;
+		infix = inf;
+		CheckInfix();
 	}
   bool CheckChars() // Проверка на допустимые символы
   {
@@ -63,7 +63,7 @@ public:
 				  && (var.find(str[i + 1]) >= 0 && var.find(str[i - 1]) <= var.length()))
 				  continue;
 			  else
-				  return false;
+				  throw "the number of variables does not correspond to the numbers of operations";
 		  }
 	  return true;
   }
@@ -92,32 +92,18 @@ public:
   }
   bool CheckInfix() // Общая проверка
   {
-	  if (CheckChars() == false) 
-		  return false;
+	  CheckChars();
+	  CheckAmount(infix);
+	  CheckBrackets(infix);
 	  if (infix[0] == ')' || infix[0] == '*' || infix[0] == '/' || infix[0] == '-' || infix[0] == '+')
-		  return false;
+		  throw "First character is operations";
 	  if (infix[infix.length()] == '(' || infix[infix.length()] == '*' || infix[infix.length()] == '/' || infix[infix.length()] == '-' || infix[infix.length()] == '+')
-		  return false;
-	  if (CheckAmount(infix) == false)
-		  return false;
-	  if (CheckBrackets(infix) == false)
-		  return false;
-
+		  throw "Last character is operations";
 	  return true;
   }
-  string DeleteSpace(string inf) // Удаление пробелов
+  void ArrVarible() // Выделение из строки переменных и добавление их в массив
   {
-	  infix = inf;
-	  for (int i = 0; i < infix.size(); i++)
-		  if (infix[i] == ' ')
-			  infix.erase(i, 1);
-  }
-  string AddEqual (string inf) // Добавление равно в конец строки
-  {
-	  return inf = inf + "=";
-  }
-  void ArrVarible(string inf) // Выделение из строки переменных и добавление их в массив
-  {
+	  string inf = infix;
 	  size_t size = 0;
 	  CheckInfix();
 	  inf = inf + "=";
@@ -155,13 +141,16 @@ public:
 		  }
 	  }
   }
-  
+  string* GetArrVar()
+  {
+	  return variable;
+  }
   string ToPostfix() 
   {
 	  string inf = infix;
 	  postfix = "";
 	  CheckInfix();
-	  ArrVarible(inf);
+	  ArrVarible();
 	  inf = inf + "=";
 	  TStack<char> operations(MaxSizeString);
 	  int i = 0;
--- Смертин/base_test/test_tpostfix.cpp
+++ Смертин/base_test/test_tpostfix.cpp
@@ -3,6 +3,98 @@
 
 TEST(TPostfix, can_create_postfix)
 {
-  ASSERT_NO_THROW(TPostfix p);
+	string inf = "a+b";
+
+	ASSERT_NO_THROW(TPostfix p(inf));
 }
 
+TEST(TPostfix, throw_when_entered_invalid_characters_in_expression)
+{
+	string inf = "a+b#@";
+
+	ASSERT_ANY_THROW(TPostfix p(inf));
+}
+
+TEST(TPostfix, throw_when_number_of_variables_does_not_correspond_to_the_numbers_of_operations)
+{
+	string inf = "a+b+";
+
+	ASSERT_ANY_THROW(TPostfix p(inf));
+}
+
+TEST(TPostfix, throw_when_incorrect_number_of_brackets)
+{
+	string inf = "((a+b)";
+
+	ASSERT_ANY_THROW(TPostfix p(inf));
+}
+
+TEST(TPostfix, throw_when_first_character_is_operations)
+{
+	string inf = "+a+b";
+
+	ASSERT_ANY_THROW(TPostfix p(inf));
+}
+
+TEST(TPostfix, throw_when_last_character_is_operations)
+{
+	string inf = "a+b-";
+
+	ASSERT_ANY_THROW(TPostfix p(inf));
+}
+
+TEST(TPostfix, can_use_ArrVarible)
+{
+	string inf = "a+b+c";
+	TPostfix p(inf);
+
+	ASSERT_NO_THROW(p.ArrVarible());
+}
+
+TEST(TPostfix, can_use_ArrVarible_2)
+{
+	string inf = "a+b+c";
+	string arrVar[3];
+	arrVar[0] = "a";
+	arrVar[1] = "b";
+	arrVar[2] = "c";
+	TPostfix p(inf);
+
+	for (int i = 0; i < 3; i++)
+		EXPECT_EQ(arrVar[i] == p.GetArrVar()[i], true);
+}
+
+TEST(TPostfix, can_use_ToPostfix)
+{
+	string inf = "a+b+c";
+	TPostfix p(inf);
+
+	ASSERT_NO_THROW(p.ToPostfix());
+}
+
+TEST(TPostfix, can_use_ToPostfix_2)
+{
+	string inf = "a+b+c";
+	string post = "ab+c+";
+	TPostfix p(inf);
+
+	EXPECT_EQ(p.ToPostfix() == post, true);
+}
+
+TEST(TPostfix, can_use_Calculate)
+{
+	string inf = "a+b+c";
+	TPostfix p(inf);
+
+	ASSERT_NO_THROW(p.Calculate());
+}
+/*
+TEST(TPostfix, can_use_Calculate_2)
+{
+	string inf = "a+b";
+	string post = "ab+c+";
+	TPostfix p(inf);
+
+	EXPECT_EQ(p.ToPostfix() == post, true);
+}
+*/
