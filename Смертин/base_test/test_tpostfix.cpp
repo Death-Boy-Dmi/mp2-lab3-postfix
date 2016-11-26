c32d81e80dee1182fb29c98d88b9d512b69578d4
Death-Boy-Dmi <smertindmi@mail.ru>
2016-11-26 18:54:11 +0300

    Test Postfix

--- Смертин/base_test/test_tpostfix.cpp
+++ Смертин/base_test/test_tpostfix.cpp
@@ -3,6 +3,68 @@
 
 TEST(TPostfix, can_create_postfix)
 {
-  ASSERT_NO_THROW(TPostfix p);
+	string inf = "a+b";
+
+	ASSERT_NO_THROW(TPostfix p(inf));
 }
 
+TEST(TPostfix, throw_when_entered_invalid_characters_in_expression)
+{
+	string inf = "a+b#@";
+
+	ASSERT_ANY_THROW(TPostfix p(inf));
+}
+
+TEST(TPostfix, throw_when_number_of_variables_does_not_correspond_to_the_numbers_of_operations)
+{
+	string inf = "a++b";
+
+	ASSERT_ANY_THROW(TPostfix p(inf));
+}
+
+TEST(TPostfix, throw_when_incorrect_number_of_brackets)
+{
+	string inf = "((a+b)))";
+
+	ASSERT_ANY_THROW(TPostfix p(inf));
+}
+
+TEST(TPostfix, throw_when_first_character_is_operations)
+{
+	string inf = "+a+b";
+
+	ASSERT_ANY_THROW(TPostfix p(inf));
+}
+
+TEST(TPostfix, throw_when_last_character_is_operations)
+{
+	string inf = "a+b+";
+
+	ASSERT_ANY_THROW(TPostfix p(inf));
+}
+
+TEST(TPostfix, can_use_ArrVarible)
+{
+	string inf = "a+b+c";
+	TPostfix p(inf);
+
+	ASSERT_NO_THROW(p.ArrVarible());
+}
+
+TEST(TPostfix, can_use_ToPostfix)
+{
+	string inf = "a+b+c";
+	TPostfix p(inf);
+
+	ASSERT_NO_THROW(p.ToPostfix());
+}
+
+
+TEST(TPostfix, can_use_Calculate)
+{
+	string inf = "a+b+c";
+	TPostfix p(inf);
+	p.ToPostfix();
+
+	ASSERT_NO_THROW(p.Calculate());
+}
