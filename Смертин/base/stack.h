66a0cf86d9303781f3d9282cd461878edc9a24d2
Death-Boy-Dmi <smertindmi@mail.ru>
2016-11-26 18:43:43 +0300

    Revert "Изменил stack.h Пробую исправить ошибку с гитом"
    This reverts commit 3306604e7257dae609466be7b8206c0c291f9c3d.

--- Смертин/base/stack.h
+++ Смертин/base/stack.h
@@ -1,47 +1,59 @@
 ﻿#ifndef __STACK_H__
 #define __STACK_H__
 
-const int MaxStackSize = 100;
+const int MaxStackSize = 1000;
 
 template <class T>
 class TStack
 {
-	T *pMem;
-	int size;
-	int top;
+  T *pMem;
+  int size;
+  int top;
 public:
-	TStack(int _size)
-	{
-		size = _size;
-		top = -1;
-		if ((size < 1) || (size > MaxStackSize))
-			throw size;
-		pMem = new T[size];
-	}
-	~TStack()
-	{
-		delete[] pMem;
-	}
-	bool IsEmpty()
-	{
-		return top == (-1);
-	}
-	bool IsFull()
-	{
-		return top == (size - 1);
-	}
-	void Put(T val)
-	{
-		if (IsFull())
-			throw val;
-		pMem[++top] = val;
-	}
-	T Get()
-	{
-		if (IsEmpty())
-			throw 0;
-		return pMem[top--];
-	}
+  TStack(int _size)
+  {
+    size = _size;
+    top = -1;
+    if ((size < 1) || (size > MaxStackSize))
+      throw size;
+    pMem = new T[size];
+  }
+  ~TStack()
+  {
+    delete [] pMem;
+  }
+  bool IsEmpty()
+  {
+	  return top == (-1);
+  }
+  bool IsFull()
+  {
+	  return top == (size - 1);
+  }
+  void Put(T val)
+  {
+	  if (IsFull())
+		  throw val;
+	  pMem[++top] = val;
+  }
+  T Get()
+  {
+	  if (IsEmpty())
+		  throw 0;
+	  return pMem[top--];
+  }
+  int GetTop()
+  {
+	  return top;
+  }
+  int GetSize()
+  {
+	  return size;
+  }
+  T GetValTop()
+  {
+	  return pMem[top];
+  }
 };
 
 #endif
